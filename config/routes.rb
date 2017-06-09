22cc1acb062e266b5e7d3b5a9ff9668fc282c6a6
veranyc <veranyc@gmail.com>
2017-06-09 11:37:56 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  get 'students', to: 'static#students'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
